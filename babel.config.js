74ef8ba719d4d538b64455dd6b6941c46061bd71
Lukas Saltenas <lukas.saltenas@gmail.com>
2025-11-24 17:59:25 +0200

    chore: update babel config by adding module resolver

--- babel.config.js
+++ babel.config.js
@@ -6,4 +6,21 @@ module.exports = {
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-react',
   ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'slate': './packages/slate/src',
+          'slate-history': './packages/slate-history/src',
+          'slate-hyperscript': './packages/slate-hyperscript/src',
+          'slate-react': './packages/slate-react/src',
+          '@deepnote/slate': './packages/slate/src',
+          '@deepnote/slate-history': './packages/slate-history/src',
+          '@deepnote/slate-hyperscript': './packages/slate-hyperscript/src',
+          '@deepnote/slate-react': './packages/slate-react/src',
+        },
+      },
+    ],
+  ],
 }
